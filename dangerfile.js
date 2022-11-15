5a2424d2f96561e9e3de9b9833d8c870eba0fe46
David Warrington <davidmwarrington+github@gmail.com>
2022-11-15 21:32:41 +0000

    feat(ci): warn if a non-md file is edited

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,22 @@
-import { message, danger } from "danger";
+import { danger, message, warn } from "danger";
 
 const createList = (prefix) => (items) => {
   return `${prefix}\n- ${items.join("\n- ")}`;
 };
+
+const allFiles = [
+  ...danger.git.created_files,
+  ...danger.git.modified_files,
+  ...danger.git.deleted_files,
+];
+const nonMdFiles = allFiles.filter((file) => !file.endsWith(".md"));
+
+if (nonMdFiles.length > 0) {
+  warn(
+    `Looks like you edited some non-md files. Be careful!` +
+      nonMdFiles.join(" - ")
+  );
+}
 
 message(createList("Created files in this PR:")(danger.git.created_files));
 message(createList("Changed files in this PR:")(danger.git.modified_files));
